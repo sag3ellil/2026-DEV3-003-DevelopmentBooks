065611c14614aa34d36c6d0b79a2ed8022777eaf
sag3ellil <bouraoui.zich@gmail.com>
2026-02-02 13:50:46 +0100

    test: implement initial test for api/book/discounts exposure

---
+++ src/test/java/com/store/books/controller/BookControllerTest.java
@@ -0,0 +1,51 @@
+package com.store.books.controller;
+
+
+import com.store.books.service.BookService;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = BookController.class)
+public class BookControllerTest {
+
+    private final MockMvc mockMvc;
+
+    @MockitoBean
+    private BookService bookService;
+
+    BookControllerTest(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    @Test
+    void shouldReturn200_whenBasketIsValid() throws Exception {
+        given(bookService.calculateMinimumPrice(any()))
+                .willReturn(95.00);
+
+        String jsonRequest = """
+                {
+                  "books": [
+                    { "bookId": 1, "quantity": 1 },
+                    { "bookId": 2, "quantity": 1 }
+                  ]
+                }
+                """;
+
+        mockMvc.perform(post("/api/book/discounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.price").value(95.00));
+    }
+
+
+}
